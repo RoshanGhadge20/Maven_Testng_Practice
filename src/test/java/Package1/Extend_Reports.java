35d5a71bb1d120237a44543ba51742e286d4d57d
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-04-21 19:06:41 +0530

    working with extent report

---
+++ src/test/java/Package1/Extend_Reports.java
@@ -0,0 +1,50 @@
+package Package1;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Extend_Reports {
+	 ExtentReports ex;
+	
+
+	public void  Reportgeneration() {
+		new ExtentReports();
+		String path=System.getProperty("User.dir")+"ExtentReport.html";
+		ExtentSparkReporter reporter=new ExtentSparkReporter(path);
+		WebDriverManager.chromedriver().setup();
+		//WebDriver driver=new ChromeDriver();
+		
+		reporter.config().setReportName("Web Automation Report");
+		reporter.config().setDocumentTitle("Extent Report ResultSet");
+		
+		ex.attachReporter(reporter);
+		ex.setSystemInfo("QA Eng.", "Roshan Ghadge");
+	
+	}
+	
+	@Test
+	public void getdata()
+	{
+		ExtentTest ts=ex.createTest("getdata");
+		System.out.println("Calling getdata method");
+		ex.flush();
+	}
+	
+	/*
+	 * @Test public void setdata() { ExtentTest ts1=ex.createTest("Setdata");
+	 * System.out.println("Calling setdata method"); ex.flush(); }
+	 * 
+	 * @Test public void showdata() { ExtentTest ts2=ex.createTest("showdata");
+	 * System.out.println("Calling sedata method"); ex.flush(); }
+	 */
+	
+	/*
+	 * @AfterClass public void tearDown() { //driver.quit(); ex.flush(); }
+	 */
+}
