a6d936a399bc7a2f107c87df1381065f1f237018
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-06-08 12:39:48 +0530

    extent report practice

--- src/test/java/Package1/AppTest5.java
+++ src/test/java/Package1/AppTest5.java
@@ -1,27 +1,50 @@
 package Package1;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
 public class AppTest5 {
+    ExtentReports er;
 
-	@Test(timeOut = 4000)
-	public void test15() {
-		System.out.println("Calling test method with timeout parameter of 4000");
-	}
+    @BeforeClass
+    public void generateReport() {
+        er = new ExtentReports();
+        String path = System.getProperty("user.dir") + "/ExtentReportdemo.html";
+        ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+        reporter.config().setDocumentTitle("QA Report");
+        reporter.config().setReportName("Extent Report ResultSet");
+        er.attachReporter(reporter);
+    }
 
-	@Test(enabled = false)
-	public void test16() {
-		System.out.println("Calling test 16 method with test execution as false");
-	}
+    @Test(timeOut = 4000)
+    public void test15() {
+        ExtentTest et = er.createTest("test15");
+        System.out.println("Calling test method with timeout parameter of 4000");
+        er.flush();
+    }
 
-	
-	@Test(dependsOnMethods = "test15")
-	public void test17() {
-		System.out.println("Calling dependent method in testng ");
-	}
-	
-	@Test(alwaysRun = true)
-	public void test18() {
-			System.out.println("Calling to always run method");
-	}
+    @Test(enabled = false)
+    public void test16() {
+        ExtentTest et = er.createTest("test16");
+        System.out.println("Calling test 16 method with test execution as false");
+        er.flush();
+    }
+
+    @Test(dependsOnMethods = "test15")
+    public void test17() {
+        ExtentTest et = er.createTest("test17");
+        System.out.println("Calling dependent method in testng ");
+        er.flush();
+    }
+
+    @Test(alwaysRun = true)
+    public void test18() {
+        ExtentTest et = er.createTest("test18");
+        System.out.println("Calling to always run method");
+        er.flush();
+    }
 }
