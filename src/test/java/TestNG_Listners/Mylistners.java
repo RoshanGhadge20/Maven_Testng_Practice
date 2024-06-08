f010863497a4030dcc2a1291e4c2333e143362a6
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-06-08 16:55:57 +0530

    ITestListners Methods

--- src/test/java/TestNG_Listners/Mylistners.java
+++ src/test/java/TestNG_Listners/Mylistners.java
@@ -12,14 +12,19 @@ public class Mylistners implements ITestListener {
 	public void onTestFailure(ITestResult result) {
 		System.out.println("Method failed :- " + result.getMethod());
 	}
-	
+
 	public void onTestSuccess(ITestResult result) {
-		System.out.println("Method passed :- "+ result.getName());
+		System.out.println("Method passed :- " + result.getName());
 	}
-	
-	public void onFinish(ITestResult result)
-	{
-		System.out.println("All the test case execution has completed "+ result.getMethod());
+
+	public void onFinish(ITestResult result) {
+		System.out.println("All the test case execution has completed " + result.getMethod());
 	}
 
 }
+
+//Methods of ITestListners 
+/*
+ * 1. OnTestStart() 2. OnTestSuccess() 3. OnTestFailure() 4. OnTestSkipped() 5.
+ * OnTestFailedButWithinSuccessPercentage() 6. OnStart() 7. OnFinish()
+ */
