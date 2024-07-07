9aa13978a2ae5647c5806751aaac596f6212cdbc
RoshanGhadge20 <roshanghadge568@gmail.com>
2024-07-07 17:13:17 +0530

    parameterization & few imp concepts

--- src/test/java/Invocations/Invocation_count_example.java
+++ src/test/java/Invocations/Invocation_count_example.java
@@ -2,17 +2,22 @@ package Invocations;
 
 import org.testng.annotations.Test;
 
-public class Invocation_count_example 
-{
+public class Invocation_count_example {
+	// Invocation count => For how many of times this method should get invoked
+	// InvocationTimeout => Within the how much of time period this all method runs
+	// should get happens
+	// Skip failed invocations => This it will check that whether the method should
+	// do continue to the execution
+	// if the attribute is set to the "true". if attribute is set to the false then
+	// it will stop the execution of test
+
 	@Test(invocationCount = 10, invocationTimeOut = 500)
-	public void m1()
-	{
+	public void m1() {
 		System.out.println("Calling Test Method");
 	}
-	
+
 	@Test(skipFailedInvocations = true)
-	public void m2()
-	{
+	public void m2() {
 		System.out.println("Calling Test Method");
 	}
 }
---
+++ src/test/java/paramertization_example/example.java
@@ -0,0 +1,16 @@
+package paramertization_example;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class example {
+
+	@Test
+	@Parameters({ "Username", "Password" })
+	public void demo(String Username, String Password) {
+		System.out.println("Example of parameterization");
+		System.out.println("Username :- " + Username);
+		System.out.println("Password :- " + Password);
+	}
+
+}
